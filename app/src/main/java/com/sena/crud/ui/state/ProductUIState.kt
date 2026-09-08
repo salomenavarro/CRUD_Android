50584860210d514d4460a776e590749040d9deff
Salome Navarro Pineda <navarrosalome16@gmail.com>
2026-09-07 23:19:38 -0500

    feat: agregar logica de actualizacion al ViewModel

---
+++ app/src/main/java/com/sena/crud/ui/state/ProductUIState.kt
@@ -0,0 +1,12 @@
+package com.sena.crud.ui.state
+
+import com.sena.crud.domain.model.ProductModel
+
+data class ProductUIState(
+    val isLoading: Boolean = false,
+    val product: ProductModel? = null,
+    val errorMessage: String? = null,
+    val isUpdating: Boolean = false,
+    val updateSuccess: Boolean = false
+)
+
